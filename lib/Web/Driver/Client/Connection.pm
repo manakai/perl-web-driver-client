7b16aa6605f99891c4b0e6bf3e22a545aa39e8f8
Wakaba <wakaba@suikawiki.org>
2017-03-09 14:05:39 +0900

    Check JSON structure more strictly

--- lib/Web/Driver/Client/Connection.pm
+++ lib/Web/Driver/Client/Connection.pm
@@ -56,8 +56,14 @@ sub new_session ($;%) {
   })->then (sub {
     my $res = $_[0];
     die $res if $res->is_error;
+    my $json = $res->json;
+    my $session_id = $json->{sessionId};
+    if (defined $json->{value} and ref $json->{value} eq 'HASH' and
+        defined $json->{value}->{sessionId}) {
+      $session_id = $json->{value}->{sessionId};
+    }
     return Web::Driver::Client::Session->new_from_connection_and_session_id
-        ($self, $res->json->{value}->{sessionId} || $res->json->{sessionId});
+        ($self, $session_id);
   });
 } # new_session
 
